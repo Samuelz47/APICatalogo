5a4dc7a3fa42fe26f481a0797c1afaaa58b388a8
Samuel Gomes <samuelgms.lt@gmail.com>
2025-08-28 22:07:05 -0300

    Starting products controller

---
+++ APICatalogo/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using APICatalogo.Domain.Entities;
+using APICatalogo.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> Get()                         //ActionResult funciona como um tipo de retorno pra aceitar o NotFound caso o retorno não seja um Enumerable<Product>
+    {
+        var products = _context.Products.ToList();                          //Cria a lista de produtos, onde usamos Enumerable pois consome menos memoria nesse caso
+        if (products is null)
+        {
+            return NotFound("Produtos não encontrados");
+        }
+        return products;
+    }
+}
